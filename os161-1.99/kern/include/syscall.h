28f0c9597b4d454656aa4aeccce71eaa5fca414f
BergesIrani <iraniber@hotmail.com>
2019-06-22 17:00:59 -0400

    lm

--- os161-1.99/kern/include/syscall.h
+++ os161-1.99/kern/include/syscall.h
@@ -29,6 +29,8 @@
 
 #ifndef _SYSCALL_H_
 #define _SYSCALL_H_
+#include "opt-A2.h"
+
 
 
 struct trapframe; /* from <machine/trapframe.h> */
@@ -63,7 +65,7 @@ int sys_write(int fdesc,userptr_t ubuf,unsigned int nbytes,int *retval);
 void sys__exit(int exitcode);
 int sys_getpid(pid_t *retval);
 int sys_waitpid(pid_t pid, userptr_t status, int options, pid_t *retval);
-int sys_fork(void);
+int sys_fork(struct trapframe *tf, pid_t *ret);
 
 
 #endif // UW
