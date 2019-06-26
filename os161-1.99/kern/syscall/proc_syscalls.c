bc8754140cebde3b506948248b84da45bb5cf1a1
BergesIrani <iraniber@hotmail.com>
2019-06-26 17:16:57 -0400

    k

--- os161-1.99/kern/syscall/proc_syscalls.c
+++ os161-1.99/kern/syscall/proc_syscalls.c
@@ -40,9 +40,6 @@ void sys__exit(int exitcode) {
           lock_release(child->lk);
       }
       lock_release(p->parent->lk);
-      /* detach this thread from its process */
-      /* note: curproc cannot be used after this call */
-      proc_remthread(curthread);
       thread_exit();
   } else {
   }
