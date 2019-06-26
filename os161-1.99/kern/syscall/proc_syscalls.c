9447344c161280ecc7946d13cbf7bb752e8f662d
BergesIrani <iraniber@hotmail.com>
2019-06-26 15:23:15 -0400

    k

--- os161-1.99/kern/syscall/proc_syscalls.c
+++ os161-1.99/kern/syscall/proc_syscalls.c
@@ -126,7 +126,7 @@ sys_waitpid(pid_t pid,
         lock_acquire(child2->lk);
         if (pid == child2->pid) {
             while(child2->exit_code == -1) {
-                lock_release(hild2->lk);
+                lock_release(child2->lk);
                 cv_wait(child2->terminating, curproc->lk);
             }
             exitstatus = _MKWAIT_EXIT(child2->exit_code);
