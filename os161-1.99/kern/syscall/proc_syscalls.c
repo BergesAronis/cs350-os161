a62d3c34dcef9a7016ab749f119dbc902b9b4650
BergesIrani <iraniber@hotmail.com>
2019-06-26 16:29:37 -0400

    k

--- os161-1.99/kern/syscall/proc_syscalls.c
+++ os161-1.99/kern/syscall/proc_syscalls.c
@@ -42,29 +42,28 @@ void sys__exit(int exitcode) {
       lock_release(p->parent->lk);
   } else {
 
+      DEBUG(DB_SYSCALL,"Syscall: _exit(%d)\n",exitcode);
+
+      KASSERT(curproc->p_addrspace != NULL);
+      as_deactivate();
+      /*
+       * clear p_addrspace before calling as_destroy. Otherwise if
+       * as_destroy sleeps (which is quite possible) when we
+       * come back we'll be calling as_activate on a
+       * half-destroyed address space. This tends to be
+       * messily fatal.
+       */
+      as = curproc_setas(NULL);
+      as_destroy(as);
+
+      /* detach this thread from its process */
+      /* note: curproc cannot be used after this call */
+      proc_remthread(curthread);
+
+      /* if this is the last user process in the system, proc_destroy()
+         will wake up the kernel menu thread */
+      proc_destroy(p);
   }
-
-  DEBUG(DB_SYSCALL,"Syscall: _exit(%d)\n",exitcode);
-
-  KASSERT(curproc->p_addrspace != NULL);
-  as_deactivate();
-  /*
-   * clear p_addrspace before calling as_destroy. Otherwise if
-   * as_destroy sleeps (which is quite possible) when we
-   * come back we'll be calling as_activate on a
-   * half-destroyed address space. This tends to be
-   * messily fatal.
-   */
-  as = curproc_setas(NULL);
-  as_destroy(as);
-
-  /* detach this thread from its process */
-  /* note: curproc cannot be used after this call */
-  proc_remthread(curthread);
-
-  /* if this is the last user process in the system, proc_destroy()
-     will wake up the kernel menu thread */
-  proc_destroy(p);
 
   thread_exit();
   /* thread_exit() does not return, so we should never get here */
