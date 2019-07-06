97a578c4da8f77ba3311962743a024af7fef2438
BergesIrani <iraniber@hotmail.com>
2019-07-06 13:26:43 -0400

    a2b first try

--- os161-1.99/kern/syscall/proc_syscalls.c
+++ os161-1.99/kern/syscall/proc_syscalls.c
@@ -206,3 +206,67 @@ sys_fork(struct trapframe *tf, pid_t *ret) {
 
 
 }
+
+int
+sys_execv(char *progname) {
+    struct addrspace *as;
+    struct vnode *v;
+    vaddr_t entrypoint, stackptr;
+    int result;
+
+    // Count the number of arguments
+
+    // Copy the program path into the kernel
+    size_t progname_size = sizeof(char) * (strlen(progname) + 1);
+    char * progname_kern = kmalloc(progname_size);
+    copyin((const_userptr_t) progname, (void *) progname_kern, progname_size);
+
+    /* Open the file. */
+    result = vfs_open(progname, O_RDONLY, 0, &v);
+    if (result) {
+        return result;
+    }
+
+
+    /* Create a new address space. */
+    as = as_create();
+    if (as ==NULL) {
+        vfs_close(v);
+        return ENOMEM;
+    }
+
+    /* Switch to it and activate it. */
+    curproc_setas(as);
+    as_activate();
+
+    /* Load the executable. */
+    result = load_elf(v, &entrypoint);
+    if (result) {
+        /* p_addrspace will go away when curproc is destroyed */
+        vfs_close(v);
+        return result;
+    }
+
+    /* Done with the file now. */
+    vfs_close(v);
+
+    /* Define the user stack in the address space */
+    result = as_define_stack(as, &stackptr);
+    if (result) {
+        /* p_addrspace will go away when curproc is destroyed */
+        return result;
+    }
+
+
+
+
+
+
+    /* Warp to user mode. */
+    enter_new_process(0 /*argc*/, NULL /*userspace addr of argv*/,
+                      stackptr, entrypoint);
+
+    /* enter_new_process does not return. */
+    panic("enter_new_process returned\n");
+    return EINVAL;
+}
