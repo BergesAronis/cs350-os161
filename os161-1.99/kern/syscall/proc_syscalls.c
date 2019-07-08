253b2c8e6edd94a0daaa26d322e5d16836abc653
BergesIrani <iraniber@hotmail.com>
2019-07-08 12:42:50 -0400

    a big attempt

--- os161-1.99/kern/syscall/proc_syscalls.c
+++ os161-1.99/kern/syscall/proc_syscalls.c
@@ -289,11 +289,11 @@ sys_execv(char *progname, char **args) {
         new_arguments[i] = new_stack;
     }
 
-    for (int i = args_many; i >= 0; --i) {
-        size_t ptr_size = sizeof(vaddr_t);
-        new_stack -= ptr_size;
-        copyout((void *) &new_arguments[i], (userptr_t) new_stack, ptr_size);
-    }
+//    for (int i = args_many; i >= 0; --i) {
+//        size_t ptr_size = sizeof(vaddr_t);
+//        new_stack -= ptr_size;
+//        copyout((void *) &new_arguments[i], (userptr_t) new_stack, ptr_size);
+//    }
 
     // Delete old adress space
     as_destroy(elder);
