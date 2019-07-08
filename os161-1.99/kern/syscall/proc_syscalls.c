01e105ec2ca49f198765fd3b8902488fe817b660
BergesIrani <iraniber@hotmail.com>
2019-07-08 12:33:00 -0400

    a big attempt

--- os161-1.99/kern/syscall/proc_syscalls.c
+++ os161-1.99/kern/syscall/proc_syscalls.c
@@ -281,15 +281,15 @@ sys_execv(char *progname, char **args) {
     vaddr_t *new_arguments = kmalloc(sizeof(vaddr_t) * (args_many + 1));
     new_arguments[args_many] = (vaddr_t) NULL;
 
-    for (int i = args_many - 1; i >= 0; --i) {
+    for (int i = (args_many - 1); i >= 0; --i) {
         size_t new_arg_len = ROUNDUP(strlen(arg_kern[i]) + 1, 4);
         size_t new_arg_size = sizeof(char) * new_arg_len;
         new_stack -= new_arg_size;
-        copyout((void *) arg_kern[i], (userptr_t) new_stack, new_arg_len)
+        copyout((void *) arg_kern[i], (userptr_t) new_stack, new_arg_len);
         new_arguments[i] = new_stack;
     }
 
-    for (int i = args_many; i >= 0 --i) {
+    for (int i = args_many; i >= 0; --i) {
         size_t ptr_size = sizeof(vaddr_t);
         new_stack -= ptr_size;
         copyout((void *) &new_arguments[i], (userptr_t) new_stack, ptr_size);
